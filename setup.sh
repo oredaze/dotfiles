0d3f1b0470f25ec4676ae83ff2ad320ba0ef4a03
oredaze <51076873+oredaze@users.noreply.github.com>
2020-05-08 18:16:34 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+chmod +x .bin/* \
+	.config/herbstluftwm/autorun.sh \
+	.config/herbstluftwm/autostart \
+	.config/polybar/scripts/* \
+	.config/ranger/colorschemes/* \
+	.config/ranger/scope.sh
+
+mv * ..
+cd ..
+rm -r dotfiles
+print "Log out, back in and startx"
